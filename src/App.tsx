49366b3d051bd7939c864be3d7847b3483d84063
Kevin <arthurkevin1260@gmail.com>
2023-11-11 23:12:13 +0000

    first typescript project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Todos from "./Todo"
+
+
+const  App=()=> {
+
+
+  
+  return (
+
+<div>
+<Todos/>
+
+</div>
+    )
+}
+
+export default App
---
+++ src/Todo.tsx
@@ -0,0 +1,15 @@
+const Todos = () => {
+    return (
+        <div  className="">
+ 
+ <header className="flex flex-row items-center justify-between px-14 py-4">           <h1 className="text-[22px] font-semibold">Todos</h1>
+
+<span className=" ">
+            <button className="rounded-md border-[1px] w-[180%] p-2 bg-blue-600 text-white border-blue-600">Add</button>
+            </span>
+            </header>
+        </div>
+      );
+}
+ 
+export default Todos;
